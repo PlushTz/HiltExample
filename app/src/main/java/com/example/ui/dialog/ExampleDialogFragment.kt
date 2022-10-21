94a4310bd368e31c37a25e884c0a2dd00f36cf86
Jeremy <44286967+Air-Rodo@users.noreply.github.com>
2022-10-21 16:31:50 +0800

    Commit DialogFragment

---
+++ app/src/main/java/com/example/ui/dialog/ExampleDialogFragment.kt
@@ -0,0 +1,72 @@
+package com.example.ui.dialog
+
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.util.Log
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.DialogFragment
+import com.example.hilt.R
+import com.example.hilt.databinding.DialogExampleBinding
+
+/**
+ * Desc:
+ * @author lijt
+ * Created on 2022/10/21 10:06
+ * Email: lijt@eetrust.com
+ */
+class ExampleDialogFragment : DialogFragment() {
+
+    companion object {
+        const val TAG = "ExampleDialogFragment"
+    }
+
+    lateinit var binding: DialogExampleBinding
+
+    private var mOnDialogClickListener: OnDialogClickListener? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d("TAG", "onCreate")
+        initWindow()
+    }
+
+    private fun initWindow() {
+        val window = dialog?.window
+        window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        window?.attributes?.width = 1000
+        window?.attributes?.height = 300
+        window?.attributes?.gravity = Gravity.CENTER
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        Log.d("TAG", "onCreateView")
+        binding = DialogExampleBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        Log.d("TAG", "onCreateDialog")
+        val view = LayoutInflater.from(requireContext())
+            .inflate(R.layout.dialog_example, null)
+        return AlertDialog.Builder(requireContext())
+            .setView(view)
+            .create()
+    }
+
+    fun setOnDialogClickListener(listener: OnDialogClickListener) {
+        mOnDialogClickListener = listener
+    }
+
+    interface OnDialogClickListener {
+        fun onCommit()
+
+        fun onCancel()
+    }
+}
--- app/src/main/java/com/example/ui/running/RunningFragment.kt
+++ app/src/main/java/com/example/ui/running/RunningFragment.kt
@@ -1,12 +1,19 @@
 package com.example.ui.running
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import com.example.hilt.databinding.FragmentRunningBinding
+import com.example.net.ApiRetrofit
+import com.example.net.GitHubApi
+import com.example.ui.dialog.ExampleDialogFragment
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 /**
  * Desc:
@@ -15,10 +22,46 @@ import dagger.hilt.android.AndroidEntryPoint
  * Email: lijt@eetrust.com
  */
 @AndroidEntryPoint
-class RunningFragment:Fragment() {
-    private lateinit var binding:FragmentRunningBinding
+class RunningFragment : Fragment() {
+    private lateinit var binding: FragmentRunningBinding
+
+    @Inject
+    lateinit var apiRetrofit: ApiRetrofit
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        init()
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = FragmentRunningBinding.inflate(inflater, container, false)
+        initListener()
         return binding.root
+    }
+
+    private fun initListener() {
+        binding.btnRequest.setOnClickListener {
+            lifecycleScope.launch {
+                apiRetrofit.searchRepos("okhttp")
+            }
+        }
+
+        binding.btnShowDialog.setOnClickListener {
+            val dialog = ExampleDialogFragment()
+            dialog.show(childFragmentManager, ExampleDialogFragment.TAG)
+            dialog.setOnDialogClickListener(object : ExampleDialogFragment.OnDialogClickListener {
+                override fun onCommit() {
+                    Log.d("TAG", "onCommit")
+                }
+
+                override fun onCancel() {
+                    Log.d("TAG", "onCancel")
+                }
+            })
+        }
+    }
+
+    private fun init() {
+
     }
 }
